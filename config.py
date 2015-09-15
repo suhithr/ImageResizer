ff479f043733d8541b3022eae4d6e121778ba96b
suhithr <suhithrajesh@yahoo.com>
2015-09-15 15:25:27 +0530

    migrated to sqllite db

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ class BaseConfig(object):
 	DEBUG = False
 	MAX_CONTENT_LENGTH = 16 * 1024 * 1024 * 1024
 	#Its pointing to the database imageresizer belonging to the user postgres
-	SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost/imageresizer'
+	SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+	# SQLALCHEMY_DATABASE_URI = 'sqlite://///home/suhith/Coding/ImageResizer/test.db'
 	ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'png'])
 	DIMENSIONS = 500
 
